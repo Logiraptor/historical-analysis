b4574688f051fe61b0fc8471f3ed100d7a93cf6b
Patrick Oyarzun <patrickoyarzun@gmail.com>
2017-05-28 21:45:03 -0400

    Fix bug in test script

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ docker build -t repo ../repo
 
 docker run --name analysis-master -v /var/run/docker.sock:/var/run/docker.sock hist python main.py $@
 
-DATE=`date +%Y-%m-%d:%H:%M:%S`
+DATE=`date +%Y-%m-%d-%H-%M-%S`
 
 docker cp analysis-master:/app/output.csv $DATE.csv
 
